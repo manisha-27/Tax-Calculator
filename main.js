b976c72049c4561a7720dc2a8b286f3ed5464d29
Manisha Singh <67019423+manisha-27@users.noreply.github.com>
2024-04-15 15:42:00 +0530

    Add Comments and Help icon tooltip to main.js

--- main.js
+++ main.js
@@ -5,13 +5,9 @@ var deduction=document.getElementById("deduction");
 
 var deductionValue = parseFloat(deduction.value) || 0;
 
+//Tax Calculator Formula
 function calculate(){
-    console.log("=====")
-    console.log(gross.value+" - "+extra.value+" - "+ageSelect.value+" - "+deduction.value)
-    console.log("=====")
-    console.log(document.getElementById("numberField").value);
     var total = parseFloat(gross.value) + parseFloat(extra.value) - parseFloat(deduction.value);
-    console.log("=====")
     var ans=0;
     if(total>800000){
         if(ageselect.value==1){
@@ -23,16 +19,16 @@ function calculate(){
         }
     }
     document.getElementById("result").innerText=total-ans;
-    console.log(ans+" in calculate() ");
     saveNoOfObjectInStorage();  // will save the number of object in local Storage (index for each row).
     saveData(total-ans);    // will save the data in local storage as well as generate row table in html.
 }
 
+
 let dataStorage=[];
 
+// Get index and no. of Calculation in total have been done
 function saveNoOfObjectInStorage(){
     let noOfObject;
-    console.log("Heyooo")
     if(localStorage.getItem("noOfObject")===null){
         noOfObject=0;
     }else{
@@ -41,6 +37,7 @@ function saveNoOfObjectInStorage(){
     localStorage.setItem("noOfObject",noOfObject+1);
 }
 
+// Save Data to Local Browser Storage and also generate it's row in table to show in History's Modal
 function saveData(ans){
     const inputData={
         gross: gross.value,
@@ -49,19 +46,16 @@ function saveData(ans){
         deduction: deductionValue,
         ans: ans
     }
-    console.log(ans+" in saveData() ");
     dataStorage.push(inputData);
     localStorage.setItem("taxData", JSON.stringify(dataStorage));
     addRowInTable(inputData)
 }
 
-// push data to html from local data storage
 const data=JSON.parse(localStorage.getItem('taxData')) || [] ;
-console.log(data);
+// console.log(data);
 const dataTable=document.getElementById("tableBody");
 
 function generateTableRowUI(rowData){
-    console.log("====")
     let index=localStorage.getItem("noOfObject");
     return `
         <tr>
@@ -83,6 +77,7 @@ var checkInputGross=false;
 var checkInputExtra=false;
 var checkInputDeduction=false;
 
+// Validate Number as per that show Error Icon and tooltip
 function validateNumber(input) {
     var value = input.value;
     var errorIcon = document.getElementById("errorIcon");
@@ -91,62 +86,13 @@ function validateNumber(input) {
     if (isNaN(value)) {
         errorIcon.classList.remove("hidden");
         errorTooltip.classList.remove("hidden");
-        // checkInputGross=true;
-        // validateNumber1();
     } else {
         errorIcon.classList.add("hidden");
         errorTooltip.classList.add("hidden");
     }
 }
 
-// function validateNumber1(input) {
-//     var value = input.value;
-//     if (isNaN(value)) {
-//         checkInputExtra=true;
-//         checkIfSubmitEnabled();
-//     }
-// }
-
-// function validateNumber1() {
-//     // if (isNaN(value)) {
-//     //     checkInputExtra=true;
-//     //     checkIfSubmitEnabled();
-//     // }
-//     var input1 = document.getElementById("gross").value;
-//   var input2 = document.getElementById("extra").value;
-//   var input3 = document.getElementById("deduction").value;
-//     // var submitButton = document.getElementById("submit-btn");
-//     console.log(input1+" - "+input2+" - "+input3);
-    
-//     // // Regular expression to match only numbers
-//     // var regex = /^[0-9]*$/;
-    
-//     // // Check if all input fields contain only numbers
-//     // if (regex.test(input1) && regex.test(input2) && regex.test(input3)) {
-//     //     submitButton.disabled = false; // Enable the button
-//     // } else {
-//     //     submitButton.disabled = true; // Disable the button
-//     // }
-// }
-
-// function validateNumber2(input) {
-//     var value = input.value;
-//     if (isNaN(value)) {
-//         checkInputDeduction=true;
-//         checkIfSubmitEnabled();
-//     }
-// }
-
-function checkIfSubmitEnabled(){
-    console.log("Heyyooooooooooooooo")
-    if(checkInputDeduction && checkInputExtra && checkInputGross){
-        var submitbtn =document.getElementById("submit-btn");
-        // submitbtn.classList.remove("disabled");
-        submitbtn.disabled=true;
-    }
-}
-
-
+// Error Icon and Tooltip
 document.getElementById("errorIcon").addEventListener("mouseover", function() {
     var tooltip = document.getElementById("errorTooltip");
     tooltip.style.display = "block";
@@ -157,16 +103,20 @@ document.getElementById("errorIcon").addEventListener("mouseout", function() {
     tooltip.style.display = "none";
 });
 
+// Help Icon and Tooltip
+var helpIcons = document.getElementsByClassName("help-icon");
 
-document.getElementById("help-icon").addEventListener("mouseover", function() {
-    var tooltip = document.getElementById("helpTooltip");
+for (var i = 0; i < helpIcons.length; i++) {
+  helpIcons[i].addEventListener("mouseover", function() {
+    var tooltip = this.nextElementSibling;
     tooltip.style.display = "block";
-});
+  });
 
-document.getElementById("help-icon").addEventListener("mouseout", function() {
-    var tooltip = document.getElementById("helpTooltip");
+  helpIcons[i].addEventListener("mouseout", function() {
+    var tooltip = this.nextElementSibling;
     tooltip.style.display = "none";
-});
+  });
+}
 
 
 // Make Submit Button eneable or Disable
@@ -206,7 +156,7 @@ ageSelect.addEventListener('change', function() {
   submitButton.disabled = !shouldEnableSubmitButton();
 });
 
-// Optionally, you can also add a check when the page loads to see if the submit button should be enabled
+// Add a check when the page loads to see if the submit button should be enabled
 document.addEventListener('DOMContentLoaded', function() {
   submitButton.disabled = !shouldEnableSubmitButton();
 });
